cf520497bfb3e3b2f8756c32d410ea976d7943be
hisusant <spsusantkumar@gmail.com>
2022-08-04 16:33:29 +0530

    mongobd code

--- mongo_insert_file.py
+++ mongo_insert_file.py
@@ -1,16 +1,23 @@
 import pymongo
 import json
 client = pymongo.MongoClient("mongodb+srv://susant:susant123@cluster0.8beoc.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-print(db)
+#db = client.test
+#print(db)
 
 database = client['ineuron2']
 collection = database['fitbit2']
 
 # opening file in r mode and load with json
-with open('fitbit.json', 'r') as fp:
-    fitbit_data = json.load(fp)
-    #print(fitbit_data)
+dataset = open('fitbit.json', 'r')
+dataset2 = json.load(dataset)
+#print(dataset2)
 
 # insert dataset
-collection.insert_one(fitbit_data)
+collection.insert_one(dataset2)
+
+'''
+# insert_many use when we have list data: 
+dataset2 = json.load(dataset)
+dataset3=[dataset2]
+collection.insert_many(dataset3)
+'''
